ac52649c764ae52115c1f23f64208f7bcdbb0936
shree-hb <107175680+shree-hb@users.noreply.github.com>
2023-06-29 02:12:58 +0530

    typo fix

--- lib/content/version.rb
+++ lib/content/version.rb
@@ -3,11 +3,3 @@
 module Content
   VERSION = "0.1.2"
 end
-
-Rails.application.config.content_engine = {
-  content_models: [ 'Story', 'Recipe' ],
-  application_name: "Demo App ",
-  main_theme_color: "#8A6E93",
-  secondary_theme_color: "#8A6E931"
-}
-
