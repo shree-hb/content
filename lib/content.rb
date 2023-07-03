0e81611e0b5fceaa5da68bc5f24738f94834b4c2
shree-hb <107175680+shree-hb@users.noreply.github.com>
2023-07-03 15:49:48 +0530

    version upgrade

--- lib/content.rb
+++ lib/content.rb
@@ -1,6 +1,9 @@
 # frozen_string_literal: true
 
 require_relative "content/version"
+
+require_relative "content/version"
+
 require_relative "content/routes"
 require_relative "content/content_crud"
 
