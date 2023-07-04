f698b4ea04ae670fbeebf10fae3822ced41048c3
shree-hb <107175680+shree-hb@users.noreply.github.com>
2023-07-04 13:32:22 +0530

    version upgrade

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,12 @@ p '---- GEM ROUTES FILE LOAD from CONFIG-ROUTES -------'
 Content::Engine.routes.draw do
 p '------- inside gem route from CONFIG-ROUTES -------'
   resources :cruds
+  
+  controller 'crud' do
+    match '/content/cruds',  :to => :index    
+  end
+  
+  get '/content/cruds' => 'cruds#index'
 
   root "cruds#index"
 
--- lib/content/engine.rb
+++ lib/content/engine.rb
@@ -1,6 +1,9 @@
 module Content
   class Engine < ::Rails::Engine
     isolate_namespace Content
+    
+    config.load_path += Dir[File.join(File.dirname(__FILE__), '..', '..', 'config').to_s]
+
   end
 end
 
